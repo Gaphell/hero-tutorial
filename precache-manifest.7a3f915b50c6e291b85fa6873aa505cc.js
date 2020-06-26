b11e796814d644acb0730ebc2a99081dfa3d5e63
Tenzin Gaphel <tenzingaphel@selise.ch>
2020-06-26 13:16:33 +0600

    Updates

---
+++ precache-manifest.7a3f915b50c6e291b85fa6873aa505cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3654edbe3ce48dfec364cbe38a3fd7f",
+    "url": "/hero-tutorial/index.html"
+  },
+  {
+    "revision": "d50a84cbe7f34c0640fd",
+    "url": "/hero-tutorial/static/css/main.d18bcce9.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/hero-tutorial/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hero-tutorial/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d50a84cbe7f34c0640fd",
+    "url": "/hero-tutorial/static/js/main.6fedd6bd.chunk.js"
+  },
+  {
+    "revision": "32ea3d2483043c6e2fe1",
+    "url": "/hero-tutorial/static/js/runtime-main.32686080.js"
+  }
+]);
